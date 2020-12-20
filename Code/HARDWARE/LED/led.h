e339e6cf935101cadde6c641f544deee4c195305
WRS_Home <1352509846@qq.com>
2020-12-20 20:16:18 +0800

    Feat: <完成USART串口功能>
    实现串口功能---2020-12-20
    - 实现串口1（PA9--PA10）收发数据功能
    - 实现  printf  函数功能
    - 完善RGB灯的翻转闪烁功能

--- Code/HARDWARE/LED/led.h
+++ Code/HARDWARE/LED/led.h
@@ -2,8 +2,14 @@
 #define __LED_H_
 #include "stm32f4xx.h"
 #include "stm32f4xx_conf.h"
+#include "sys.h"
 
-void LED_GPIO_Config(void);
+#define LED_R_TOGGLE PAout(0)=~PAout(0)
+#define LED_G_TOGGLE PAout(1)=~PAout(1)
+#define LED_B_TOGGLE PAout(2)=~PAout(2)
+
+void LED_GPIO_Init(void);
+
 
 
 
--- Code/USER/main.c
+++ Code/USER/main.c
@@ -1,15 +1,16 @@
 #include "stm32f4xx.h"
 #include "led.h"
 #include "delay.h"
+#include "usart.h"
 int main(void)
 {
-	LED_GPIO_Config();
-	delay_init(100);
+	LED_GPIO_Init();
+	delay_init(100);//主频100MHz
+	Usart1_Init(9600);
 	while(1)
 	{
-		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		delay_ms(100);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-		delay_ms(100);
+		LED_R_TOGGLE;		
+		delay_ms(200);
+		printf("hello_world\r\n");
 	}
 }
